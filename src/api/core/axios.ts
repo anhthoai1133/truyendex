312483f0e8d94a3fad0ea68da47a5206d307287f
anhthoai1133 <70251722+anhthoai1133@users.noreply.github.com>
2025-02-28 02:10:24 +0700

    Config to base 64

--- src/api/core/axios.ts
+++ src/api/core/axios.ts
@@ -6,6 +6,7 @@ const axios = Axios.create({
   baseURL: Constants.BACKEND_URL,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
+    "Origin": Constants.APP_URL,
   },
   withCredentials: true,
   withXSRFToken: true,
@@ -13,8 +14,18 @@ const axios = Axios.create({
 
 axios.interceptors.request.use(
   (config) => {
-    // Dynamically set the baseURL before each request
-    config.baseURL = Utils.Url.getBackendUrl();
+    // Get base URL
+    const baseUrl = Utils.Url.getBackendUrl();
+    
+    // Encode the URL if using CORS proxy
+    if (baseUrl.includes("services.f-ck.me")) {
+      const fullUrl = `${Constants.BACKEND_URL}${config.url}`;
+      config.url = Utils.Url.encodeBase64Url(fullUrl);
+      config.baseURL = `${Constants.CORS_URL}/v1/cors`;
+    } else {
+      config.baseURL = baseUrl;
+    }
+    
     return config;
   },
   (error) => {
